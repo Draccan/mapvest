b9b4c80a2b99e3e1921069d8032309572d09276c
Paolo Dell'Aguzzo <guzzopaul@gmail.com>
2025-10-29 19:12:33 +0100

    fix: migration

--- findInMap-backend/drizzle/0000_silent_puppet_master.sql
+++ findInMap-backend/drizzle/0000_silent_puppet_master.sql
@@ -1,5 +1,10 @@
-CREATE TYPE "public"."MapPointType" AS ENUM('THEFT', 'AGGRESSION', 'ROBBERY');
-CREATE TABLE "map_points" (
+DO $$ BEGIN
+ CREATE TYPE "public"."MapPointType" AS ENUM('THEFT', 'AGGRESSION', 'ROBBERY');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE IF NOT EXISTS "map_points" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"location" geometry(Point, 4326) NOT NULL,
 	"type" "MapPointType" NOT NULL,
@@ -7,7 +12,7 @@ CREATE TABLE "map_points" (
 	"created_at" timestamp (3) DEFAULT now() NOT NULL,
 	"updated_at" timestamp (3) DEFAULT now() NOT NULL
 );
-CREATE TABLE "users" (
+CREATE TABLE IF NOT EXISTS "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar NOT NULL,
 	"surname" varchar NOT NULL,
