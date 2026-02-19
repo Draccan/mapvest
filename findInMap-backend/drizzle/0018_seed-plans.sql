6d8363898d7c20b91d897cbf13a52ae20cd6dad0
Paolo Dell'Aguzzo <guzzopaul@gmail.com>
2026-02-19 16:30:42 +0100

    feat: seed base plans

---
+++ findInMap-backend/drizzle/0018_seed-plans.sql
@@ -0,0 +1,4 @@
+-- Custom SQL migration file, put your code below! --
+
+INSERT INTO "plans" ("name", "max_map_points") VALUES ('free', 50);
+INSERT INTO "plans" ("name", "max_map_points") VALUES ('pro', 15000);
