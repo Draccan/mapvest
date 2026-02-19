1e14eb27a07ca320ef9dc24a242fb1022d76557b
Paolo Dell'Aguzzo <guzzopaul@gmail.com>
2026-02-19 17:53:54 +0100

    fix: not seed

--- findInMap-backend/drizzle/0018_seed-plans.sql
+++
@@ -1,4 +0,0 @@
--- Custom SQL migration file, put your code below! --
-
-INSERT INTO "plans" ("name", "max_map_points") VALUES ('free', 50);
-INSERT INTO "plans" ("name", "max_map_points") VALUES ('pro', 15000);
